57db82ab0080c7e9b1246e71c5496fcd460a55d4
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-10-19 01:15:31 +0600

    imported config

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from config import open_weather_token
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_weather(city, open_weather_token):
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    city = 'Almaty'
+    get_weather(city, open_weather_token)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
